cdc9577cd24e2c53414e16bdc3983de555c8c40f
Karuhito <179291384+Karuhito@users.noreply.github.com>
2025-12-23 15:12:44 +0900

    2025-12-23 15:12 ログイン機能の実装、その他ファイルの調整

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 User = get_user_model()
 
@@ -7,3 +9,13 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username',)
+
+class JapaneseAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='ユーザーネーム',
+        widget=forms.TextInput(attrs={'class' : 'form-control'})
+    )
+    password = forms.CharField(
+        label='パスワード',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import SignupView
 from django.contrib.auth.views import LoginView, LogoutView
+from .forms import JapaneseAuthenticationForm
 
 app_name = 'accounts'
 
@@ -8,6 +9,6 @@ urlpatterns = [
     # signup
     path('signup/', SignupView.as_view(), name='signup'),
     # login/logout
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(authentication_form=JapaneseAuthenticationForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
