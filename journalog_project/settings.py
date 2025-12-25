c28a26f83d10578f91abb6df3f54af1ffb25eeb1
Karuhito <179291384+Karuhito@users.noreply.github.com>
2025-12-25 17:02:01 +0900

    2025-12-25 17:01 render公開するにあたりrequimentsとsettingsとREADMEを編集

--- journalog_project/settings.py
+++ journalog_project/settings.py
@@ -1,25 +1,20 @@
 from pathlib import Path
-import os 
+import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",
+]
 
 
 # Application definition
